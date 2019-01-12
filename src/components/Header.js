ea72f9473776e0ae7eb683ff664b6370f1f75200
pjhl <pjhl.coder@gmail.com>
2019-01-12 14:46:50 +0200

    Updated links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/lib/Navbar'
 import Nav from 'react-bootstrap/lib/Nav'
 
 const Header = () => (
   <header>
     <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-      <Navbar.Brand href='/'>Bootstrap admin</Navbar.Brand>
+      <Link className='navbar-brand' to='/'>Bootstrap admin</Link>
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='mr-auto'>
-          <Nav.Link href='/signin'>Signin</Nav.Link>
-          <Nav.Link href='/signup'>Signup</Nav.Link>
+          <Link className='nav-link' to='/signin'>Signin</Link>
+          <Link className='nav-link' to='/signup'>Signup</Link>
         </Nav>
         <Nav>
+          <Link className='nav-link' to='#'>One</Link>
           <Nav.Link href='#'>One</Nav.Link>
           <Nav.Link eventKey={2} href='#'>
             Two
